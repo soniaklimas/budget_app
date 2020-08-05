3d617ad19eb760dece03ca54145034fa7f68977a
Sonia Klimas <sonia.klimas@gmail.com>
2020-08-05 16:02:14 +0200

    implement absolute imports

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navigation() {
+  return <div>Text</div>;
+}
+
+export default Navigation;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,2 @@
+// podaję ścieżkę tylko do katalogu, nie muszę podawać do pliku
+export { default } from "./Navigation";
