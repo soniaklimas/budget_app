28ded5606dc8ffd7a5c829cb0ef9dffd02104241
Sonia Klimas <sonia.klimas@gmail.com>
2020-08-19 15:10:24 +0200

    implement useMemo hook in Button component, optimisation

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { InlineButton, RegularButton } from "./Button.css";
 
 function Button({ variant, children, ...props }) {
-  //nie może być 'type' bo jest zarezerwowany przez html
+  //nie może być 'type' bo jest zarezerwowany przez html, stąd 'variant'
   // wyciągam "to" z ...props
   const { to } = props;
+
+  // useMemo - jako argument funkcja, której rezultat chcemy przypisać do zmiennej (Component), [] - kiedy funkcja wywołana ponownie, []=kiedy komponent renderuje się po raz pierwszy, ma wywołać tę funkcję i już więcej jej nie wywoływać z
   // by komponent był tym, co zwraca funkcja w zależności od case switcha, należy funkjcę od razu wywołać = owrapować w nawiasy i wywołać ()
-  const Component = (() => {
+  const Component = useMemo(() => {
     switch (variant) {
       case "inline":
         return InlineButton;
@@ -16,15 +19,17 @@ function Button({ variant, children, ...props }) {
       default:
         return RegularButton;
     }
-  })();
+  }, [variant]);
 
-  return to ? (
-    <Link {...props}>
-      <Component {...props}>{children}</Component>
-    </Link>
-  ) : (
-    <Component {...props}>{children}</Component>
-  );
+  const content = useMemo(() => <Component {...props}>{children}</Component>, [
+    props,
+    children,
+  ]);
+
+  return to ? <Link {...props}>{content}</Link> : <>{content}</>;
 }
 
+Button.propTypes = {
+  variant: PropTypes.oneOf(["inline", "regular"]),
+};
 export default Button;
